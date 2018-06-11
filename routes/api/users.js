cb90e2558d378a793c068b3aaf8fcc2c2acbaf87
Steven <codebusiness2014@gmail.com>
2018-06-11 16:21:50 -0400

    Adding Bcrypt to password. Checking login user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,77 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+
+// Load User model
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Tests user route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+// @route   GET api/users/register
+// @desc    Register a user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route   GET api/users/register
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find User by Email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(400).json({ email: "User not found" });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload.
+        // Sign Token
+        jwt.sign();
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
+  });
+});
 
 module.exports = router;
