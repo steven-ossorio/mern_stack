8382cebae83fc85c626778fbb6dd696e8f4927c7
Steven <codebusiness2014@gmail.com>
2018-06-26 17:20:32 -0400

    Fixed  a minor problem regarding returning a span

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,39 +1,34 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
-    const { profile } = this.props.profile;
+    const { profile } = this.props;
+
+    // Get first name
+    const firstName = profile.user.name.trim().split(" ")[0];
+
+    // Skill List
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check">{skill}</i>
+      </div>
+    ));
 
     return (
       <div class="row">
         <div class="col-md-12">
           <div class="card card-body bg-light mb-3">
-            <h3 class="text-center text-info">{profile.user.name} Bio</h3>
+            <h3 class="text-center text-info">{firstName} Bio</h3>
             <p class="lead">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Provident fuga cum necessitatibus blanditiis vel, officia facere
-              porro esse numquam assumenda doloremque saepe aliquam nemo
-              excepturi aliquid maiores! Excepturi, libero repudiandae.
+              {isEmpty(profile.bio) ? null : <span>{profile.bio}</span>}
             </p>
             <hr />
             <h3 class="text-center text-info">Skill Set</h3>
             <div class="row">
               <div class="d-flex flex-wrap justify-content-center align-items-center">
-                <div class="p-3">
-                  <i class="fa fa-check" /> HTML
-                </div>
-                <div class="p-3">
-                  <i class="fa fa-check" /> CSS
-                </div>
-                <div class="p-3">
-                  <i class="fa fa-check" /> JavaScript
-                </div>
-                <div class="p-3">
-                  <i class="fa fa-check" /> Python
-                </div>
-                <div class="p-3">
-                  <i class="fa fa-check" /> C#
-                </div>
+                {skills}
               </div>
             </div>
           </div>
