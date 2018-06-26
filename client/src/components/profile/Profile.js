acde041cd98143ea9db1469627cf4aa9230ea5a2
Steven <codebusiness2014@gmail.com>
2018-06-26 14:41:38 -0400

    A user personal Profile

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "./ProfileGithhub";
+import Spinner from "../common/Spinner";
+import { getProfileByHandle } from "../../actions/profileActions";
+
+class Profile extends Component {
+  componentDidMount() {
+    if (this.props.match.params.handle) {
+      let handle = this.props.match.params.handle;
+      this.props.getProfileByHandle(handle);
+    }
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+Profile.propTypes = {
+  getProfileByHandle: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  profile: state.profile;
+};
+
+export default connect(
+  mapStateToProps,
+  { getProfileByHandle }
+)(Profile);
