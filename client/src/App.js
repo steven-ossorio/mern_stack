3bdebc7d1c07e3fcad79f54af9d88afecde265bc
Steven <codebusiness2014@gmail.com>
2018-06-27 22:37:52 -0400

    Added a not-found page for when an incorrect URL is accessed

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -91,6 +92,7 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
