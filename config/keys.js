94676d40fc5404ac89ecfd04c8c29c946a836598
Steven <codebusiness2014@gmail.com>
2018-07-08 20:12:06 -0400

    Prepare for heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://brad:brad12@ds055485.mlab.com:55485/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
